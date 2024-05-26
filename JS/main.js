f8b8a0eed7fdf0f0627786517b3a6c8ce7d82ca4
giogiosgr <giovannisgr88@gmail.com>
2024-05-26 11:55:02 +0200

    feat: BONUS (popolamento select tramite Javascript)

--- JS/main.js
+++ JS/main.js
@@ -9,13 +9,6 @@ const priceText = document.getElementById('price');
 const intSpan = document.getElementById('int');
 const decimalSpan = document.getElementById('decimal');
 
-//dichiarazione oggetto con i possibili prezzi orari
-const prices = {
-    "frontend": 20.50,
-    "backend": 15.30,
-    "analysis": 33.60
-};
-
 //dichiarazione array con i possibili codici sconto
 const codes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
@@ -39,12 +32,35 @@ function extractDecimal(num) {
     return decimal.split(".")[1];
 }
 
+//dichiarazione funzione per il popolamento dell'elemento select del DOM
+function addOptions(arr) {
+    //variabile stringa che conterrà il codice HTML man mano aggiunto
+    let htmlString = "";
+    //iterazione per ogni elemento dell'array passato come parametro alla funzione
+    arr.forEach(function (elem) {
+        //alla stringa codice viene aggiunta una option con classe e testo
+        htmlString += `<option value="${elem}">${elem}</option>`;
+    })
+    //all'elemento select viene aggiunta la stringa con le option, prima del tag di chiusura
+    selectInput.insertAdjacentHTML("beforeend", htmlString);
+}
+
+//viene richiamata la funzione per il popolamento, passando la lista dei lavori
+addOptions(["Sviluppo Backend", "Sviluppo Frontend", "Analisi Progettuale"]);
+
+//dichiarazione oggetto con i prezzi orari dei lavori possibili
+const prices = {
+    "Sviluppo Backend": 20.50,
+    "Sviluppo Frontend": 15.30,
+    "Analisi Progettuale": 33.60
+};
+
 //aggiunta evento al submit del form con funzione per il calcolo del prezzo finale
 form.addEventListener("submit", function (event) {
     //annullamento del comportamento di default del form al submit
     event.preventDefault();
 
-    //si prende il valore (numero progressivo) corrispondente al lavoro scelto
+    //si estrae il valore corrispondente al lavoro scelto dal select
     selectedJob = selectInput.value;
 
     //il valore di selectedJob è la chiave usata per assegnare il valore corrente alla variabile dedicata
@@ -53,19 +69,22 @@ form.addEventListener("submit", function (event) {
     //calcolo del prezzo finale
     fullPrice = currentHourPrice * jobHours;
 
-    //si estrae il valore del codice inserito
+    //si estrae il valore del codice promozionale inserito, SE inserito
     usedCode = codeInput.value;
 
-    //condizione in cui verifichiamo che il codice usato sia uno di quello validi
-    if (codes.includes(usedCode)) {
-        //se sì, allora il prezzo finale è scontato del 25%
-        fullPrice -= fullPrice * (discount / 100);
-        //nel caso il messaggio di errore fosse stato reso visibile in precedenza, viene ora nascosto
-        warningText.classList.add("d-none");
-    }
-    //altrimenti, l'elemento con il messaggio di errore nel DOM viene reso visibile, SE il campo non è vuoto
-    else if (usedCode !== "") {
-        warningText.classList.remove("d-none");
+    //nel caso il messaggio di errore fosse stato reso visibile in precedenza, viene nascosto nuovamente
+    warningText.classList.add("d-none");
+    //condizione di verifica che sia stato effettivamente inserito un codice
+    if (usedCode !== "") {
+        //condizione di verifica che il codice usato sia uno di quello validi
+        if (codes.includes(usedCode)) {
+            //se sì, allora il prezzo finale è scontato del 25%
+            fullPrice -= fullPrice * (discount / 100);
+        }
+        //altrimenti, l'elemento con il messaggio di errore nel DOM viene reso visibile
+        else {
+            warningText.classList.remove("d-none");
+        }
     }
 
     //il prezzo viene arrotondato a due cifre decimali
@@ -74,11 +93,11 @@ form.addEventListener("submit", function (event) {
     //estrazione della parte intera del prezzo
     intPrice = parseInt(fullPrice);
 
-    //estrazione della parte decimale del prezzo (richiamando la funzione dedicata)
+    //estrazione della parte decimale del prezzo, richiamando la funzione creata
     decimalPrice = extractDecimal(fullPrice);
 
     //assegnazione delle parti intera e decimale del prezzo agli elementi dedicati del DOM
-    intSpan.innerText = `€${intPrice}`;
+    intSpan.innerText = `€ ${intPrice}`;
     decimalSpan.innerText = `,${decimalPrice}`;
 
     //il container del prezzo viene reso visibile
